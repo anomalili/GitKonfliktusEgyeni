e26a82edc8b41f10630b4323032b24619bedbc62
anomalili <makrazsofi@gmail.com>
2022-03-07 14:43:02 +0100

    1.név inicializálva

---
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -0,0 +1,13 @@
+
+package konfliktuskezeles;
+
+public class KonfliktusKezeles {
+
+    public static void main(String[] args) {
+        String nev1= "MZS";
+        String nev2;
+        
+        
+    }
+    
+}
