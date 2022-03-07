bba2811ae0ee19b8ebdb75f84563e8757b1b25ed
anomalili <makrazsofi@gmail.com>
2022-03-07 14:49:51 +0100

    3. nev deklalalasa

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -5,7 +5,8 @@ public class KonfliktusKezeles {
 
     public static void main(String[] args) {
         String nev1= "MZS";
-        String nev2 = "WW";     
+        String nev2 = "WW";
+        String nev3;
     }
     
 }
