f40e572ba85ab62f204423ade5c60213f5637577
mzsl <95681537+anomalili@users.noreply.github.com>
2022-03-07 14:46:50 +0100

    név 3 deklarása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -4,8 +4,10 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        int i = 7;
         String nev1= "MZS";
-        String nev2 = "WW";     
+        String nev2 = "WW";
+        int nev3;
     }
     
 }
